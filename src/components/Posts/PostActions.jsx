ce237bcb66f8c9d9541823703410bbdc2eb593e6
CorpusOwO <corpuscivilis@protonmail.com>
2023-08-16 18:21:08 -0400

    finished postStats

--- src/components/Posts/PostActions.jsx
+++ src/components/Posts/PostActions.jsx
@@ -1,29 +1,56 @@
-import React from 'react'
-import {FaComment, FaRetweet, FaHeart, FaChartSimple, FaUpload} from 'react-icons/fa6'
+import {
+  FaComment,
+  FaRetweet,
+  FaHeart,
+  FaChartSimple,
+  FaUpload,
+} from "react-icons/fa6";
 function PostActions() {
-return (
-<div className="text-gray-500 dark:text-gray-400 flex mt-3 justify-around">
-    <div className="flex items-center mr-6 ">
-        <FaComment />
-        <span className="ml-1">615</span>
+  return (
+    <div className="text-gray-500 dark:text-gray-400 flex mt-3 justify-around ">
+      <div className="flex items-center mr-6 group hover:cursor-pointer ">
+        <span className="p-2 rounded-3xl group-hover:bg-[#1d9cf025] transition ease-in-out delay-[35ms]">
+          <FaComment className="group-hover:[#1d9cf025]" />
+        </span>
+        <span className="ml-2 group-hover:text-[#1d9bf0] transition ease-in-out delay-[35ms]">
+          615
+        </span>
+      </div>
+
+      <div className="flex items-center mr-6 group hover:cursor-pointer">
+        <span className=" p-2 rounded-3xl group-hover:bg-[#00ba7c67] transition ease-in-out delay-[35ms]">
+          <FaRetweet />
+        </span>
+        <span className="ml-2 group-hover:text-[#00ba7c] transition ease-in-out delay-[35ms] ">
+          93
+        </span>
+      </div>
+
+      <div className="flex items-center mr-6 group hover:cursor-pointer">
+        <span className=" p-2 rounded-3xl group-hover:bg-[#e6177870] transition ease-in-out delay-[35ms]">
+          <FaHeart />
+        </span>
+        <span className="ml-2 group-hover:text-[#e61777] transition ease-in-out delay-[35ms]">
+          103
+        </span>
+      </div>
+
+      <div className="flex items-center mr-6 group hover:cursor-pointer">
+        <span className=" p-2 rounded-3xl group-hover:bg-[#1d9cf025] transition ease-in-out delay-[35ms]">
+          <FaChartSimple />
+        </span>
+        <span className="ml-2 group-hover:text-[#1d9bf0] transition ease-in-out delay-[35ms]">
+          214
+        </span>
+      </div>
+
+      <div className="flex items-center mr-6 group hover:cursor-pointer">
+        <span className="p-2 rounded-3xl group-hover:bg-[#1d9cf025] transition ease-in-out delay-[30ms]">
+          <FaUpload />
+        </span>
+      </div>
     </div>
-    <div className="flex items-center mr-6">
-        <FaRetweet />
-        <span className="ml-3">93</span>
-    </div>
-    <div className="flex items-center mr-6">
-        <FaHeart />
-        <span className="ml-3">103</span>
-    </div>
-    <div className="flex items-center mr-6">
-        <FaChartSimple />
-        <span className="ml-3">214</span>
-    </div>
-    <div className="flex items-center mr-6">
-        <FaUpload />
-    </div>
-</div>
-)
+  );
 }
 
-export default PostActions
+export default PostActions;
--- src/components/users/SmallUserProfile.jsx
+++ src/components/users/SmallUserProfile.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-
-function SmallUserProfile({userPic, name, username}) {
+function SmallUserProfile({
+  userPic = "https://unavatar.io/CorpusOwO",
+  name = "default",
+  username = "default",
+}) {
   return (
-    <div className='flex gap-4'>
-    <img src={userPic} alt="profile user pic" className=' w-12 h-12 rounded-full' />
-    <div>
-      <p className='font-semibold'>{name}</p>
-      <small className='text-gray-400'> @{username} </small>
+    <div className="flex gap-4">
+      <img
+        src={userPic}
+        alt="profile user pic"
+        className=" w-12 h-12 rounded-full"
+      />
+      <div>
+        <p className="font-semibold text-white">{name}</p>
+        <small className="text-gray-400"> @{username} </small>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default SmallUserProfile
+export default SmallUserProfile;
